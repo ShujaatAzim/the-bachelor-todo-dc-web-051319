e5e9359d79c5761a60e819a51765dce1cec23af0
Shujaat Azim <shujaat.azim@gmail.com>
2019-05-13 20:17:19 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,7 +1,8 @@
 def get_first_name_of_season_winner(data, season)
-  data[season.to_sym].each do |contestant|
-    if [contestant][:status] == "Winner"
-      return contestant[:name].split(" ")[0]
+  data[season].each do |contestant|
+    contestant.each do |info, value|
+    if [value][:status] == "Winner"
+      return value[:name].split(" ")[0]
     end
   end
 end
